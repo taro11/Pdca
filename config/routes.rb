9c42b1b468eda6eeae7889aa2917c3924c2efa62
Taro Yamaguchi <t97yamaguchi@gmail.com>
2016-11-02 13:19:57 +0900

    root設定 view作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   resources :projects, only: [:index, :show]
   get   'parts/new/1'    =>  'parts#new_1'
   get   'parts/new/2'    =>  'parts#new_2'
   get   'parts/new/3'    =>  'parts#new_3'
   get   'parts/new/4'    =>  'parts#new_4'
+  root :to => 'projects#index'
 end
